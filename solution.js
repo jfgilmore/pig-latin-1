610bd33ae1625983476346404329f6014d7f6f69
ed system on behalf of Edward Deam <ed>
2020-06-23 23:35:57 +0000

    Scaffold update by instructor

--- solution.js
+++
@@ -1,8 +0,0 @@
-function toPigLatin(phrase) {
-  // Your code here
-  if (phrase.length === 0) return '';
-
-  const convertWord = (word) => `${word.substring(1)}${word.charAt(0)}ay`;
-
-  return phrase.split(' ').map((word) => convertWord(word)).join(' ');
-}
