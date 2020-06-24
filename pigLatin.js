eacca79c874b0b2ee3335a3f3ef59100cf872c8e
Joshua Gilmore <djsounddog@gmail.com>
2020-06-24 10:53:48 +1000

    all tests passing, bahaving as expected

--- pigLatin.js
+++ pigLatin.js
@@ -1,7 +1,22 @@
 function toPigLatin(phrase) {
   // Your code here
+  if (phrase === "") {
+    return "";
+  }
+  words = phrase.split(" ");
+
+  words.forEach((word, i) => {
+    word += word[0];
+    word = word.substr(1 - word.length);
+    word += "ay";
+    words[i] = word;
+  });
+
+  return words.join(" ");
 }
 
 module.exports = {
   toPigLatin,
 };
+
+toPigLatin("doggy has a bone");
