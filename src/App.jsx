0ac9bccdfa598c10812302909918efb163652e39
Joshua Gilmore <djsounddog@gmail.com>
2020-06-24 13:30:19 +1000

    imported toPigLatin into main react App

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
-import React from 'react';
+import React from "react";
+import toPigLatin from "./utils/pigLatin";
 
 class App extends React.Component {
-
-  pigLatin = (english) => {
-    // https://edstem.org/courses/4230/challenges/15985
-    // logic for translating english to pig latin
-    // return something
-    return "ishay iglay"
-  }
-
   handleSubmit = (event) => {
-    event.preventDefault()
-    const english = event.target[0].value
-    const result = this.pigLatin(english)
-    this.setState({ pigLatin: result })
-  }
+    event.preventDefault();
+    const english = event.target[0].value;
+    const result = toPigLatin(english);
+    this.setState({ pigLatin: result });
+  };
 
   render() {
-    const pigLatin = this.state?.pigLatin
+    const pigLatin = this.state?.pigLatin;
     return (
       <div className="container">
         <h1>Convert english to pig latin! 🐷</h1>
-        <form onSubmit={this.handleSubmit} >
+        <form onSubmit={this.handleSubmit}>
           <textarea name="pig" id="pig"></textarea>
-          <input type="submit" value="Convert"/>
+          <input type="submit" value="Convert" />
         </form>
         <h1>Output</h1>
         <div className="output">
